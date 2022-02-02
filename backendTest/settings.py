2e8ba1644da8a31afc4d937e7474d1429b68aabe
jay <jimniz01@gmail.com>
2022-02-01 22:30:16 -0500

    initial

--- backendTest/settings.py
+++ backendTest/settings.py
@@ -127,10 +127,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-   
-]
 MEDIA_URL = str(os.path.join(BASE_DIR, "static/images/"))
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
